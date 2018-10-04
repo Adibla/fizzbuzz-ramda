9e2d5f7c1b0094ded09b35934e47fd9da87e0d41
Andrea <andrea.diblasix@gmail.com>
2018-10-04 16:12:11 +0200

    Updated

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const R = require('ramda');
 const fizzbuzz = (num) => {
     const moduleOperator = (a) => (b) => b % a;
     
+    const divisibleBy3 = R.compose(R.equals(0), moduleOperator(3));
+    const divisibleBy5 = R.compose(R.equals(0), moduleOperator(5));
+    
     const fn = R.cond([
-        [R.compose(R.equals(0), moduleOperator(3)), R.always('fizz')],
-        [R.compose(R.equals(0), moduleOperator(5)), R.always('buzz')],
-        [R.and(R.compose(R.equals(0), moduleOperator(5)), R.compose(R.equals(0), moduleOperator(3))), R.always('fizz buzz')],
+        [R.both(divisibleBy3, divisibleBy5), R.always('fizz buzz')],
+        [divisibleBy3, R.always('fizz')],
+        [divisibleBy5, R.always('buzz')],
         [R.T, temp => 'default ']
     ]);
     
     return fn(num);
 };
 
-console.log(fizzbuzz(15));
+fizzbuzz(15);//fizz buzz
+fizzbuzz(5); //buzz
+fizzbuzz(3); //fizz
